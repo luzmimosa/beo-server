c6f30b5a485dfd689b1897bf93d7898bdcab293d
Pau <pauete.sb@gmail.com>
2023-03-05 22:24:01 +0100

    created event logger

--- src/main/java/com/fadedbytes/beo/log/LogManager.java
+++ src/main/java/com/fadedbytes/beo/log/LogManager.java
@@ -9,17 +9,28 @@ import java.util.HashMap;
 public class LogManager {
 
     public static final NamespacedKey MAIN_LOGGER_KEY = new NamespacedKey(NamespacedKey.BEO_NAMESPACE, "main_logger");
+    public static final NamespacedKey EVENT_LOGGER_KEY = new NamespacedKey(NamespacedKey.BEO_NAMESPACE, "event_logger");
 
-    private HashMap<NamespacedKey, BeoLogger> loggers = new HashMap<>();
+    private final HashMap<NamespacedKey, BeoLogger> loggers = new HashMap<>();
 
     public LogManager() {
         createMainLogger();
+        createEventLogger();
     }
 
     private void createMainLogger() {
         if (!this.loggers.containsKey(MAIN_LOGGER_KEY)) {
             this.loggers.put(
                 MAIN_LOGGER_KEY,
+                new SimpleLogger()
+            );
+        }
+    }
+
+    private void createEventLogger() {
+        if (!this.loggers.containsKey(EVENT_LOGGER_KEY)) {
+            this.loggers.put(
+                EVENT_LOGGER_KEY,
                 new SimpleLogger()
             );
         }
@@ -35,6 +46,14 @@ public class LogManager {
         }
 
         return this.loggers.get(MAIN_LOGGER_KEY);
+    }
+
+    public @NotNull BeoLogger getEventLogger() {
+        if (!this.loggers.containsKey(EVENT_LOGGER_KEY)) {
+            createEventLogger();
+        }
+
+        return this.loggers.get(EVENT_LOGGER_KEY);
     }
 
 }
