739cf5ebf1ffbf1b3eaf7bd4c953b71f0a091a9e
Pau <pauete.sb@gmail.com>
2023-03-05 13:46:51 +0100

    log level for loggers

---
+++ src/main/java/com/fadedbytes/beo/log/LogLevel.java
@@ -0,0 +1,20 @@
+package com.fadedbytes.beo.log;
+
+public enum LogLevel {
+
+    DEBUG(1000),
+    ERROR(900),
+    WARN(800),
+    INFO(200),
+    EVENT(100);
+
+    private final int level;
+    LogLevel(int level) {
+        this.level = level;
+    }
+
+    public boolean isAtLeast(LogLevel otherLevel) {
+        return this.level >= otherLevel.level;
+    }
+
+}
