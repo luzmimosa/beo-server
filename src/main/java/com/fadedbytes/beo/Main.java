b45def32a280d900fa1e1b412ba073354226446b
Pau <pauete.sb@gmail.com>
2023-03-05 20:43:22 +0100

    implemented log and console startup

--- src/main/java/com/fadedbytes/beo/Main.java
+++ src/main/java/com/fadedbytes/beo/Main.java
@@ -1,7 +1,41 @@
 package com.fadedbytes.beo;
 
+import com.fadedbytes.beo.console.BeoConsole;
+import com.fadedbytes.beo.console.ServerConsole;
+import com.fadedbytes.beo.log.LogLevel;
+import com.fadedbytes.beo.log.LogManager;
+import com.fadedbytes.beo.util.key.NamespacedKey;
+import org.jetbrains.annotations.NotNull;
+
 public class Main {
+
+    private static LogManager logManager;
+    private static BeoConsole console;
+
     public static void main(String[] args) {
-        System.out.println("BEO server");
+        logManager = createLogManager();
+        console = configureConsole();
+
+        logManager.getMainLogger().info("Starting Beo Server...");
+
+    }
+
+    private static LogManager createLogManager() {
+        return new LogManager();
+    }
+
+    private static @NotNull BeoConsole configureConsole() {
+        BeoConsole console = new ServerConsole(
+                NamespacedKey.of("server_console"),
+                100,
+                System.out
+        );
+
+        console.addOutputStream(System.out);
+        console.bindLogger(logManager.getMainLogger(), LogLevel.DEBUG);
+
+        console.startConsole();
+
+        return console;
     }
 }
