9d96131eba1e1d3f3d6956a2dedd811135556f44
Pau <pauete.sb@gmail.com>
2023-03-06 21:54:39 +0100

    added a console alert when debug mode is activated

--- src/main/java/com/fadedbytes/beo/Main.java
+++ src/main/java/com/fadedbytes/beo/Main.java
@@ -10,15 +10,28 @@ import com.fadedbytes.beo.server.StandaloneServer;
 import com.fadedbytes.beo.util.key.NamespacedKey;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Arrays;
+
 public class Main {
 
     private static LogManager logManager;
     private static BeoConsole console;
     private static EventManager eventManager;
 
+    private static boolean isDebug;
+
     public static void main(String[] args) {
+
+        isDebug = Arrays.stream(args).anyMatch(arg -> arg.equalsIgnoreCase("--debug"));
+
         logManager = createLogManager();
         console = configureConsole();
+
+        if (isDebug) {
+            printDebugModeAlert(
+                    Arrays.stream(args).anyMatch(arg -> arg.equalsIgnoreCase("--skip-debug-delay"))
+            );
+        }
 
         logManager.getMainLogger().info("Starting Beo Server...");
 
@@ -41,10 +54,28 @@ public class Main {
         );
 
         console.addOutputStream(System.out);
-        console.bindLogger(logManager.getMainLogger(), LogLevel.DEBUG);
+        console.bindLogger(logManager.getMainLogger(), isDebug ? LogLevel.DEBUG : LogLevel.ERROR);
 
         console.startConsole();
 
         return console;
+    }
+
+    private static void printDebugModeAlert(boolean skipDelay) {
+        logManager.getMainLogger().warn("""
+                   
+                   █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+                   █ WARNING: Debug mode is enabled. This may cause performance issues and security risks. █
+                   █ The debug mode is intended for development purposes only.                             █
+                   █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█
+                    """);
+
+        if (!skipDelay) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
