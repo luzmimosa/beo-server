cd0eb5630ff385583bc3d1b83c96ef3047f81e96
Pau <pauete.sb@gmail.com>
2023-03-06 18:56:24 +0100

    represents a method that is candidate to listen to events

---
+++ src/main/java/com/fadedbytes/beo/event/listener/EventListener.java
@@ -0,0 +1,15 @@
+package com.fadedbytes.beo.event.listener;
+
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface EventListener {
+
+    ListenerPriority priority() default ListenerPriority.NORMAL;
+
+}
