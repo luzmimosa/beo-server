e45005a74280bf76c77f7f9fbb769119cc2a698c
Pau Salanova Blanco <pauete.sb@gmail.com>
2023-03-06 19:24:16 +0100

    changed the way the event manager stores listener classes

--- src/main/java/com/fadedbytes/beo/event/EventManager.java
+++ src/main/java/com/fadedbytes/beo/event/EventManager.java
@@ -13,47 +13,95 @@ import java.util.List;
 
 public class EventManager {
 
-    private final HashMap<Class<? extends Listener>, ArrayList<Method>> REGISTERED_LISTENERS;
+    private final ArrayList<RegisteredListener> REGISTERED_LISTENERS;
 
     public EventManager() {
-        this.REGISTERED_LISTENERS = new HashMap<>();
+        this.REGISTERED_LISTENERS = new ArrayList<>();
     }
 
-    public void addEventListener(Class<? extends Listener> listenerClass) {
-        if (this.REGISTERED_LISTENERS.containsKey(listenerClass)) return;
+    public synchronized void addEventListener(Class<? extends Listener> listenerClass) throws IllegalArgumentException {
+        if (this.containsListener(listenerClass)) {
+            throw new IllegalArgumentException("Listener class already registered: " + listenerClass.getName());
+        }
+
+        RegisteredListener registeredListener = new RegisteredListener(listenerClass);
+        this.REGISTERED_LISTENERS.add(registeredListener);
     }
 
-    public void removeEventListener(Class<? extends Listener> listenerClass) {
-        this.REGISTERED_LISTENERS.remove(listenerClass);
-    }
-
-    private List<Method> getValidMethods(Class<? extends Listener> listenerClass) {
-        List<Method> validMethods = new ArrayList<>();
-        for (Method method : listenerClass.getMethods()) {
-            Class<Event> eventClass = getEventTarget(method);
-            if (eventClass != null) {
-                validMethods.add(method);
+    public boolean containsListener(Class<? extends Listener> listenerClass) {
+        for (RegisteredListener registeredListener : this.REGISTERED_LISTENERS) {
+            if (registeredListener.getListenerClass().equals(listenerClass)) {
+                return true;
             }
         }
-        return validMethods;
+        return false;
     }
 
-    private @Nullable Class<Event> getEventTarget(Method method) {
-        EventListener annotation = method.getAnnotation(EventListener.class);
-        if (annotation == null) return null;
+    public synchronized void removeEventListener(Class<? extends Listener> listenerClass) {
+        this.REGISTERED_LISTENERS.removeAll(
+                this.REGISTERED_LISTENERS.stream()
+                        .filter(registeredListener -> registeredListener.getListenerClass().equals(listenerClass))
+                        .toList()
+        );
+    }
 
-        Class<?>[] parameterTypes = method.getParameterTypes();
-        if (parameterTypes.length != 1) return null;
+    private static class RegisteredListener {
+        private final Class<? extends Listener> listenerClass;
+        private final HashMap<Class<Event>, Method[]> validMethods;
 
-        Class<?> parameterType = parameterTypes[0];
+        public RegisteredListener(Class<? extends Listener> listenerClass) {
+            this.listenerClass = listenerClass;
+            this.validMethods = getValidMethods(listenerClass);
 
-        if (!Event.class.isAssignableFrom(parameterType)) return null;
+            if (this.validMethods.isEmpty()) {
+                throw new IllegalArgumentException("Listener class has no valid methods: " + listenerClass.getName());
+            }
+        }
 
-        try {
-            Class<Event> eventClass = (Class<Event>) parameterType;
-            return eventClass;
-        } catch (ClassCastException e) {
-            return null;
+        public Class<? extends Listener> getListenerClass() {
+            return listenerClass;
+        }
+
+        public HashMap<Class<Event>, Method[]> getValidMethods() {
+            return new HashMap<>(validMethods);
+        }
+
+        private static HashMap<Class<Event>, Method[]> getValidMethods(Class<? extends Listener> listenerClass) {
+            HashMap<Class<Event>, Method[]> validMethods = new HashMap<>();
+
+            for (Method method : listenerClass.getMethods()) {
+                Class<Event> eventClass = getEventTarget(method);
+                if (eventClass == null) continue;
+
+                if (!validMethods.containsKey(eventClass)) {
+                    validMethods.put(eventClass, new Method[1]);
+                } else {
+                    Method[] methods = validMethods.get(eventClass);
+                    Method[] newMethods = new Method[methods.length + 1];
+                    System.arraycopy(methods, 0, newMethods, 0, methods.length);
+                    methods = newMethods;
+                    validMethods.put(eventClass, methods);
+                }
+            }
+            return validMethods;
+        }
+
+        private static @Nullable Class<Event> getEventTarget(Method method) {
+            EventListener annotation = method.getAnnotation(EventListener.class);
+            if (annotation == null) return null;
+
+            Class<?>[] parameterTypes = method.getParameterTypes();
+            if (parameterTypes.length != 1) return null;
+
+            Class<?> parameterType = parameterTypes[0];
+
+            if (!Event.class.isAssignableFrom(parameterType)) return null;
+
+            try {
+                return (Class<Event>) parameterType;
+            } catch (ClassCastException e) {
+                return null;
+            }
         }
     }
 
