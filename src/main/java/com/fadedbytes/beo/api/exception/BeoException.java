3f2cab6d06f92145a420dd2b9e06690b7d876f3f
Pau <pauete.sb@gmail.com>
2023-03-19 17:58:31 +0100

    base exception for Beo

---
+++ src/main/java/com/fadedbytes/beo/api/exception/BeoException.java
@@ -0,0 +1,10 @@
+package com.fadedbytes.beo.api.exception;
+
+/**
+ * Base class for all exceptions in the Beo project.
+ */
+public class BeoException extends Exception {
+    public BeoException(String message) {
+        super(message);
+    }
+}
