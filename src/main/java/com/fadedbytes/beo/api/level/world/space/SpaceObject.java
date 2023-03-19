7559769f614718a9e0baf221a5c6124633da4a4e
Pau <pauete.sb@gmail.com>
2023-03-19 18:01:19 +0100

    SpaceObject interface

---
+++ src/main/java/com/fadedbytes/beo/api/level/world/space/SpaceObject.java
@@ -0,0 +1,13 @@
+package com.fadedbytes.beo.api.level.world.space;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface SpaceObject {
+
+    @NotNull Vector getLocation();
+
+    @NotNull Vector getVelocity();
+
+
+
+}
